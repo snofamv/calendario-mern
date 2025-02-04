55b051bcbcd498ab3170f8dbc251696a486ff2f7
snofamv <74159423+snofamv@users.noreply.github.com>
2025-02-04 16:32:06 -0300

    fix date util

--- src/calendar/types/Date.ts
+++ src/calendar/types/Date.ts
@@ -1,19 +1,24 @@
 import { z } from "zod";
 
-const eventSchema = z.object({
-  title: z.string().min(1, "El título es obligatorio"),
-  notes: z.string().optional(),
-  start: z.date({
-    required_error: "La fecha de inicio es obligatoria",
-    invalid_type_error: "La fecha de inicio debe ser válida",
-  }),
-  end: z
-    .date({
+export const eventSchema = z
+  .object({
+    title: z.string().min(1, "El título es obligatorio"),
+    notes: z.string().optional(),
+    start: z.date({
+      required_error: "La fecha de inicio es obligatoria",
+      invalid_type_error: "La fecha de inicio debe ser válida",
+    }),
+    end: z.date({
       required_error: "La fecha de fin es obligatoria",
       invalid_type_error: "La fecha de fin debe ser válida",
-    })
-    .refine((end, ctx) => {
-      const start = ctx.parent?.start;
-      return start && end > start;
-    }, "La fecha de fin debe ser mayor que la fecha de inicio"),
-});
+    }),
+  })
+  .superRefine((data, ctx) => {
+    if (data.start >= data.end) {
+      ctx.addIssue({
+        path: ["end"], // Marcar el error en el campo "end"
+        message: "La fecha de fin debe ser mayor que la fecha de inicio",
+        code: "custom",
+      });
+    }
+  });
